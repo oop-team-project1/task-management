28818477e58e19c62cba475a0386f7a897ebc663
Kaloyan <kaloyan.enchev@abv.bg>
2023-11-15 20:30:28 +0200

    Member - added: print,viewActivity

--- src/taskmanagement/models/MemberImpl.java
+++ src/taskmanagement/models/MemberImpl.java
@@ -1,32 +1,77 @@
 package taskmanagement.models;
 
+import taskmanagement.models.contracts.Member;
 import taskmanagement.models.tasks.TaskImpl;
+import taskmanagement.models.tasks.contracts.Task;
 import taskmanagement.utils.ValidationHelper;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MemberImpl {
+public class MemberImpl implements Member {
 
     public static final String NAME_LENGTH_ERR = "Member name must be between %s and %s characters long!";
+    public static final String ADD_TASK_TO_LIST = "Task %s added to member %s";
+    public static final String HISTORY_MESSAGE = "Activity History on member %s";
     private String name;
-    private List<TaskImpl> taskImpls;
+    private List<Task> tasks;
     private List<String> activityHistory;
 
     public MemberImpl(String name) {
         setName(name);
-        taskImpls = new ArrayList<>();
+        tasks = new ArrayList<>();
         activityHistory = new ArrayList<>();
     }
 
-    private void setName(String name) {
+    private void setName(String name){
         ValidationHelper.validateStringLength(name,
-                                            TeamImpl.NAME_LEN_MIN,
-                                            TeamImpl.NAME_LEN_MAX,
-                                            String.format(NAME_LENGTH_ERR,
-                                                    TeamImpl.NAME_LEN_MIN,
-                                                    TeamImpl.NAME_LEN_MAX));
+                TeamImpl.NAME_LEN_MIN,
+                TeamImpl.NAME_LEN_MAX,
+                String.format(NAME_LENGTH_ERR,
+                        TeamImpl.NAME_LEN_MIN,
+                        TeamImpl.NAME_LEN_MAX));
         this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public List<Task> getTask() {
+        return new ArrayList<>(tasks);
+    }
+
+    @Override
+    public List<String> getActivityHistory() {
+        return new ArrayList<>(activityHistory);
+    }
+
+    @Override
+    public void addTask(Task task) {
+        tasks.add(task);
+        String message = String.format(ADD_TASK_TO_LIST, task.getTitle(), name);
+        activityHistory.add(message);
+
+    }
+
+    @Override
+    public String viewActivity() {
+        StringBuilder result = new StringBuilder(String.format(HISTORY_MESSAGE, getName()));
+        result.append(System.lineSeparator());
+        for(String activity : activityHistory) {
+            result.append(activity).append(System.lineSeparator());
+        }
+
+        return result.toString();
+    }
+
+    @Override
+    public String print() {
+        StringBuilder result = new StringBuilder();
+        result.append(getName()).append(System.lineSeparator());
+        return result.toString();
     }
 
 
