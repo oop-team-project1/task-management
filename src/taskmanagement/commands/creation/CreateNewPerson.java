7913c7e33203480ac056298d09d565b8bc2d9759
Aleksandrina Velikova <avelikova@tu-sofia.bg>
2023-11-15 17:45:19 +0200

    print() and viewActivity() methods added

--- src/taskmanagement/commands/creation/CreateNewPerson.java
+++ src/taskmanagement/commands/creation/CreateNewPerson.java
@@ -5,13 +5,16 @@ import taskmanagement.core.contracts.TaskManagementRepository;
 
 import java.util.List;
 
-public class CreateNewPerson implements Command
-{
+public class CreateNewPerson implements Command {
+
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 1;
+
     private TaskManagementRepository taskManagementRepository;
 
+    private  String name;
+
     @Override
-    public String execute(List<String> parameters)
-    {
+    public String execute(List<String> parameters) {
         return "";
     }
 }
