a6c3f24677b5ee8e756fd4473a4a51c375854c5b
Aleksandrina Velikova <avelikova@tu-sofia.bg>
2023-11-19 15:49:29 +0200

    added getString() in TestHelpers

--- test/taskmanagement/utils/TestHelpers.java
+++ test/taskmanagement/utils/TestHelpers.java
@@ -8,4 +8,7 @@ public class TestHelpers
     public static List<String> getList(int wantedSize) {
         return Arrays.asList(new String[wantedSize]);
     }
+    public static String getString(int wantedSize) {
+        return "x".repeat(wantedSize);
+    }
 }
