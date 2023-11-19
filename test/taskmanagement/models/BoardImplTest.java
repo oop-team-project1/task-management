d895a97f372aa37fb412318ad8710672df842a11
Aleksandrina Velikova <avelikova@tu-sofia.bg>
2023-11-19 16:12:00 +0200

    BoardImpl tests

---
+++ test/taskmanagement/models/BoardImplTest.java
@@ -0,0 +1,16 @@
+package taskmanagement.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import taskmanagement.utils.TestHelpers;
+
+public class BoardImplTest {
+    public static final String VALID_BOARD_NAME = TestHelpers.getString(BoardImpl.NAME_MIN_LENGTH+1);
+    public static final String INVALID_BOARD_NAME = TestHelpers.getString(BoardImpl.NAME_MAX_LENGTH+1);
+
+    @Test
+    public void constructor_Should_ThrowException_When_NameLengthOutOfBounds(){
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                new BoardImpl(INVALID_BOARD_NAME));
+    }
+}
