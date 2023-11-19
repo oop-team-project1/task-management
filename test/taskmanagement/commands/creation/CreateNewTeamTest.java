69538563abedb4d40ed2a55dc17fac7b5994a5b8
ivonasto <ivona.stoyanova.s@gmail.com>
2023-11-19 18:06:18 +0200

    Changed Junit to 5. not 4.

--- test/taskmanagement/commands/creation/CreateNewTeamTest.java
+++ test/taskmanagement/commands/creation/CreateNewTeamTest.java
@@ -1,8 +1,57 @@
 package taskmanagement.commands.creation;
 
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import taskmanagement.commands.contracts.Command;
+import taskmanagement.core.TaskManagementRepositoryImpl;
+import taskmanagement.core.contracts.TaskManagementRepository;
+import taskmanagement.models.BoardImpl;
+import taskmanagement.models.MemberImpl;
+import taskmanagement.models.TeamImpl;
+import taskmanagement.models.TeamImplTest;
+import taskmanagement.models.contracts.Board;
+import taskmanagement.models.contracts.Member;
+import taskmanagement.models.tasks.enums.Priority;
+import taskmanagement.models.tasks.enums.bug.Severity;
+import taskmanagement.utils.TestHelpers;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreateNewTeamTest {
+
+    public static final int EXPECTED_NUMBER_OF_PARAMS = 1;
+
+    private Command command;
+    private TaskManagementRepository taskManagementRepository;
+
+    @BeforeEach
+    public void before()
+    {
+        this.taskManagementRepository = new TaskManagementRepositoryImpl();
+        this.command = new CreateNewTeam(taskManagementRepository);
+    }
+
+    @Test
+    public void should_ThrowException_When_ArgumentCountDifferentThanExpected() {
+        List<String> params = TestHelpers.getList(EXPECTED_NUMBER_OF_PARAMS +1);
+        assertThrows(IllegalArgumentException.class, () -> command.execute(params));
+    }
+
+    @Test
+    public void execute_Should_CreateNewTeam_When_PassedValidInput()
+    {
+        List<String> params = List.of(TeamImplTest.VALID_NAME);
+        command.execute(params);
+
+        Assertions.assertEquals(1, taskManagementRepository.getTeams().size());
+    }
+
+
 
 
 }
