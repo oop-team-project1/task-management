720977792ec745795abcc9d59dad04bedd219296
ivonasto <ivona.stoyanova.s@gmail.com>
2023-11-20 11:31:03 +0200

    Tracking CreateStory

---
+++ test/taskmanagement/core/TaskManagementRepositoryImplTest.java
@@ -0,0 +1,98 @@
+package taskmanagement.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskManagementRepositoryImplTest {
+
+    @Test
+    void getTeams_Should_returnCopyOfCollection() {
+
+    }
+
+    @Test
+    void getMembers_Should_returnCopyOfCollection() {
+    }
+
+    @Test
+    void getTasks_Should_returnCopyOfCollection() {
+    }
+
+    @Test
+    void findMemberByName_Should_ThrowException_When_MemberDoesNotExist() {
+    }
+
+    @Test
+    void findBoardByName_Should_ThrowException_When_BoardDoesNotExist() {
+
+    }
+
+    @Test
+    void findTeamByName_Should_ThrowException_When_TeamDoesNotExist() {
+    }
+
+    @Test
+    void findStoryById_Should_ThrowException_When_StoryDoesNotExist() {
+    }
+
+    @Test
+    void findStoryById_Should_ReturnStory_When_StoryExists() {
+    }
+
+
+    @Test
+    void findFeedbackById_Should_ThrowException_When_FeedbackDoesNotExist() {
+    }
+
+    @Test
+    void findBugById_Should_ThrowException_When_BugDoesNotExist() {
+    }
+
+    @Test
+    void findTaskById_Should_ThrowException_When_TaskDoesNotExist() {
+
+    }
+
+    @Test
+    void getBoards_Should_Return_CopyOfBoards() {
+    }
+
+    @Test
+    void createNewPerson_should_AddMemberToList() {
+    }
+
+    @Test
+    void createNewTeam_should_AddTeamToTeams() {
+    }
+
+    @Test
+    void createNewBoard_should_AddBoardToBoards() {
+    }
+
+    @Test
+    void createNewBug_should_AddBugToTasks() {
+    }
+
+    @Test
+    void createNewStory_should_AddStoryToTasks() {
+    }
+
+    @Test
+    void createNewFeedback_should_AddFeedbackToTasks() {
+    }
+
+    @Test
+    void addTeam_Should_ThrowException_WhenTeamAlreadyExists() {
+    }
+
+    @Test
+    void addMember_Should_ThrowException_WhenMemberAlreadyExists() {
+
+    }
+
+    @Test
+    void addBoard_Should_ThrowException_WhenBoardAlreadyExists(){
+    }
+
+}
---
+++ test/taskmanagement/models/TeamImplTest.java
@@ -0,0 +1,41 @@
+package taskmanagement.models;
+
+import org.junit.jupiter.api.Test;
+import taskmanagement.utils.TestHelpers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TeamImplTest {
+
+    public static final int NAME_LENGTH = 15;
+
+    public static final String VALID_NAME = TestHelpers.getString(NAME_LENGTH);
+
+    @Test
+    void addBoard() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getMembers() {
+    }
+
+    @Test
+    void addMember() {
+    }
+
+    @Test
+    void removeMember() {
+    }
+
+    @Test
+    void getBoards() {
+    }
+
+    @Test
+    void print() {
+    }
+}
