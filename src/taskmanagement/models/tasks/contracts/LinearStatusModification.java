8917c529f37980ebbd926616234ee58d9baced88
ivonasto <ivona.stoyanova.s@gmail.com>
2023-11-20 11:31:20 +0200

    Revert "Add advance/revert status interface, commands and impl"
    This reverts commit 23e98bab21a4e686a3d1ef7029d23d46ac27254c.

--- src/taskmanagement/models/tasks/contracts/LinearStatusModification.java
+++
@@ -1,8 +0,0 @@
-package taskmanagement.models.tasks.contracts;
-
-public interface LinearStatusModification {
-    void advanceStatus();
-    void revertStatus();
-    String currentStatus();
-    final static String STATUS_DUPLICATION_ERR = "Status already set to %s!";
-}
