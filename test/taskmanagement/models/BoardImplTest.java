b7bd54eeafb39c68a772a7bfa7b9cf518b4e8c70
Aleksandrina Velikova <avelikova@tu-sofia.bg>
2023-11-21 12:51:45 +0200

    BoardImplTest @Before changed to @BeforeEach

--- test/taskmanagement/models/BoardImplTest.java
+++ test/taskmanagement/models/BoardImplTest.java
@@ -1,6 +1,7 @@
 package taskmanagement.models;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import taskmanagement.models.contracts.Board;
 import taskmanagement.models.tasks.FeedbackImpl;
@@ -15,53 +16,50 @@ public class BoardImplTest {
     public static final String INVALID_BOARD_NAME = TestHelpers.getString(BoardImpl.NAME_MAX_LENGTH+1);
 
     public static final int MIN_TITLE_LENGTH = 10;
-   // private final int MAX_TITLE_LENGTH = 100;
+    Board board;
 
-   // public static final String VALID_NAME_FOR_FEEDBACK = TestHelpers.getString(MIN_TITLE_LENGTH + 1)
-
+    @BeforeEach
+    public void setUp() {
+        board = initializeBoard();
+    }
     @Test
     public void constructor_Should_ThrowException_When_NameLengthOutOfBounds(){
+
         Assertions.assertThrows(IllegalArgumentException.class, () ->
                 new BoardImpl(INVALID_BOARD_NAME));
     }
 
     @Test
     public void should_Create_Board_When_ValidValuesArePassed() {
-        Board board = initializeBoard();
 
         Assertions.assertEquals(VALID_BOARD_NAME, board.getName());
     }
 
     @Test
     public void getTask_Should_ReturnCopyOfCollection() {
-        Board board = initializeBoard();
         List<Task> tasks = board.getTask();
         List<Task> sameReference = board.getTask();
 
         Assertions.assertNotSame(tasks, sameReference);
-
     }
 
     @Test
     public void getActivityHistory_Should_ReturnCopyOfCollection() {
-        Board board = initializeBoard();
         List<String> history = board.getActivityHistory();
         List<String> sameReference = board.getActivityHistory();
 
         Assertions.assertNotSame(history, sameReference);
-
     }
 
     @Test
     public void addTask_Should_AddTask_When_TaskIsValid(){
-        Board board = initializeBoard();
         Task taskToAdd = initializeTestFeedback();
+
         Assertions.assertAll(
                 () -> Assertions.assertDoesNotThrow(() -> board.addTask(taskToAdd)),
                 () -> Assertions.assertEquals(1, board.getTask().size())
         );
     }
-
 
     public static Board initializeBoard() {
         return new BoardImpl(VALID_BOARD_NAME);
