e04d8d4181b0c35a9ac6afe2cfa97ac3541cef19
ivonasto <ivona.stoyanova.s@gmail.com>
2023-11-24 13:28:57 +0200

    UnassignStoryTest impl

--- test/taskmanagement/commands/modifying/UnassignStoryTest.java
+++ test/taskmanagement/commands/modifying/UnassignStoryTest.java
@@ -6,29 +6,48 @@ import org.junit.jupiter.api.Test;
 import taskmanagement.commands.listing.ShowTeamMembers;
 import taskmanagement.core.TaskManagementRepositoryImpl;
 import taskmanagement.core.contracts.TaskManagementRepository;
+import taskmanagement.models.MemberImpl;
+import taskmanagement.models.TeamImpl;
+import taskmanagement.models.contracts.Member;
+import taskmanagement.models.contracts.Team;
+import taskmanagement.models.tasks.BugImpl;
+import taskmanagement.models.tasks.StoryImpl;
+import taskmanagement.models.tasks.contracts.Bug;
+import taskmanagement.models.tasks.contracts.Story;
+import taskmanagement.models.tasks.enums.Priority;
+import taskmanagement.models.tasks.enums.bug.Severity;
+import taskmanagement.models.tasks.enums.story.Size;
 import taskmanagement.utils.TestHelpers;
+import taskmanagement.commands.contracts.Command;
 
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 class UnassignStoryTest {
 
-    private UnassignStory unassignStory;
-    private TaskManagementRepository repository;
+    public static final int VALID_NAME_SIZE = 5;
+    public static final int VALID_TITLE_SIZE = 10;
+    public static final int VALID_DESCRIPTION_SIZE = 10;
+    public static final int STORY_ID = 1;
+
+    public static int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
+    private Command command;
+    private TaskManagementRepository taskManagementRepository;
 
     @BeforeEach
-    public void before() {
-        repository = new TaskManagementRepositoryImpl();
-        unassignStory = new UnassignStory(repository);
+    public void setUp() {
+        taskManagementRepository = new TaskManagementRepositoryImpl();
+        command = new UnassignStory(taskManagementRepository);
 
     }
 
     @Test
     public void should_ThrowException_When_ArgumentCountDifferentThanExpected() {
-        List<String> params = TestHelpers.getList(ShowTeamMembers.EXPECTED_NUMBER_OF_ARGUMENTS - 1);
+        List<String> params = TestHelpers.getList(EXPECTED_NUMBER_OF_ARGUMENTS - 1);
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> unassignStory.execute(params));
-
+        assertThrows(IllegalArgumentException.class, () -> command.execute(params));
     }
 
     @Test
@@ -41,5 +60,23 @@ class UnassignStoryTest {
         //TODO
     }
 
+
+
+
+    @Test
+    public void should_UnassignStory_When_StoryIsAssigned() {
+        //Arrange
+        List<String> params = List.of(String.valueOf(STORY_ID), TestHelpers.getString(VALID_NAME_SIZE));
+        Team team = new TeamImpl(TestHelpers.getString(VALID_NAME_SIZE));
+        Member memberAssigned = new MemberImpl(TestHelpers.getString(VALID_NAME_SIZE));
+        taskManagementRepository.addTeam(team);
+        taskManagementRepository.addMember(memberAssigned);
+        Story story = new StoryImpl(1, TestHelpers.getString(VALID_TITLE_SIZE), TestHelpers.getString(VALID_DESCRIPTION_SIZE), Priority.LOW, Size.LARGE, memberAssigned);
+        taskManagementRepository.addStory(story);
+        memberAssigned.addTask(story);
+        command.execute(params);
+        //Act and Assert
+        Assertions.assertThrows(IllegalArgumentException.class, story::getAssignee);
+    }
 
 }
