53c2f56725441618589fab5ba33947117cea1e3f
ivonasto <ivona.stoyanova.s@gmail.com>
2023-11-27 13:05:30 +0200

    CreateNewTeamTest impl

--- test/taskmanagement/commands/creation/CreateNewTeamTest.java
+++ test/taskmanagement/commands/creation/CreateNewTeamTest.java
@@ -1,8 +1,35 @@
 package taskmanagement.commands.creation;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import taskmanagement.commands.contracts.Command;
+import taskmanagement.core.TaskManagementRepositoryImpl;
+import taskmanagement.core.contracts.TaskManagementRepository;
+import taskmanagement.models.TeamImpl;
+import taskmanagement.utils.TestHelpers;
+
+import java.util.List;
+
 
 class CreateNewTeamTest {
+
+    private Command command;
+    private TaskManagementRepository taskManagementRepository;
+
+    @BeforeEach
+    public void setUp() {
+        this.taskManagementRepository = new TaskManagementRepositoryImpl();
+        this.command = new CreateNewTeam(taskManagementRepository);
+    }
+
+
+    @Test
+    void createNewTeam_Should_Create_A_NewTeam() {
+        command.execute(List.of(TestHelpers.getString(TeamImpl.NAME_LEN_MIN)));
+        Assertions.assertEquals(1, taskManagementRepository.getTeams().size());
+    }
 
 
 }
